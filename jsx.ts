d09c67a9eaf7f6aa6f28ef8461f580f5024f84d1
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2026-05-20 21:52:47 +0200

    Refactor publish workflow and update Deno configuration; remove slow types flag and adjust linting rules

--- jsx.ts
+++ jsx.ts
@@ -1,14 +1,24 @@
-declare global {
-  function jsxFactory(): JSX.Element;
-  function jsxFragmentFactory(): JSX.Element;
-
-  namespace JSX {
-    interface Element {
-      isElement: true;
-    }
-    interface ElementChildrenAttribute {
-      children?: Node;
-    }
-    type Node = Element | string | number | boolean | null | undefined | Node[];
-  }
+export function jsx(): Element {
+  throw new Error("Not implemented yet");
 }
+
+export function jsxFragment(): Element {
+  throw new Error("Not implemented yet");
+}
+
+export interface Element {
+  isElement: true;
+}
+
+export interface ElementChildrenAttribute {
+  children?: Node;
+}
+
+export type Node =
+  | Element
+  | string
+  | number
+  | boolean
+  | null
+  | undefined
+  | Node[];
